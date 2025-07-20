2f4355579b76547f14e47c8ca22281a4be9d356f
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-07-20 14:52:17 +0530

    start working on UI part

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+export default function Calendar() {
+  return (
+    <div className='calendar'>
+      <div className="calendar-header">
+        <button>← Previous</button>
+        <h2>Month Year</h2>
+        <button>Next →</button>
+      </div>
+      <div className="calendar-grid">
+        
+      </div>
+    </div>
+  )
+}
